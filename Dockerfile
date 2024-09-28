77684a1586e4c6c70e46e5e31a2775a8db29f511
Scott Schulthess <scott@Sheilaghs-MacBook-Air.local>
2024-09-28 14:28:13 -0400

    add gemfile and dockerfile

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+FROM gollumwiki/gollum:6.0.1
+COPY . .
+
--- docker-run.sh
+++ docker-run.sh
@@ -1,1 +1,1 @@
-docker run --rm -p 4567:4567 -v $(pwd):/wiki   -e GOLLUMARGS="--allow-uploads uploads" gollumwiki/gollum:master   --allow-uploads
+docker run --rm -p 4567:4567 -v $(pwd):/wiki   -e GOLLUMARGS="--allow-uploads uploads" fairysecretcompendium --allow-uploads
